9bfc0c05a9298b1a01129048838dc063cb46e5ba
Waldemar Dibiazi Junior <waldemarjr@gmail.com>
2024-03-24 15:30:33 -0300

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
-apt update
-apt install mc -y
+
+OUTPUT=/dev/ttyS0
+
+writeLog(){
+  echo $1 >> $OUTPUT
+}
+
+check(){
+  if [ $? -eq 0 ]; then
+    writeLog "update: OK"
+  else
+    writeLog "update: FAIL"
+  fi
+}
+
+apt update 1> /dev/null 2> /dev/null
+check
+
+apt install mc -y 1> /dev/null 2> /dev/null
+check
