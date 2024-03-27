29c08724633938c5321f74bc34bbd6a55631082a
Waldemar Dibiazi Junior <waldemarjr@gmail.com>
2024-03-27 19:16:09 -0300

    Update deployNomadCluster.sh

--- deployNomadCluster.sh
+++ deployNomadCluster.sh
@@ -24,23 +24,26 @@ check "update"
 apt install apt-transport-https ca-certificates curl gnupg lsb-release -y > /dev/null 2> /dev/null
 check "install_others"
 
-#install -m 0755 -d /etc/apt/keyrings 1> /dev/null 2> /dev/null 
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-#chmod a+r /etc/apt/keyrings/docker.gpg 1> /dev/null 2> /dev/null 
-#check "install_gpg_docker_repo"
+install -m 0755 -d /etc/apt/keyrings 1> /dev/null 2> /dev/null 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg 1> /dev/null 2> /dev/null 
+check "install_gpg_docker_repo"
 
-#echo \
-#  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-#  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-#  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-#check "install_docker_repo"
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+check "install_docker_repo"
 
-#apt update 1> /dev/null 2> /dev/null
+apt update 1> /dev/null 2> /dev/null
 
 sleep 5
 
-#apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin 1> /dev/null 2> /dev/null
-#check "install_docker"
+apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin 1> /dev/null 2> /dev/null
+check "install_docker"
+
+systemctl enable docker --now
+check "enable_docker"
 
 apt-get -y install  glusterfs-server glusterfs-common glusterfs-client  1> /dev/null 2> /dev/null
 check "install_glusterfs"
@@ -51,10 +54,7 @@ check "make_datadir_gluster"
 systemctl enable glusterd --now1> /dev/null 2> /dev/null
 check "start_gluster_service"
 
-#systemctl enable docker --now
-#check "enable_docker"
-
-sleep 30
+sleep 5
 
 if [ `hostname -s` == "node01" ]; then
   node02Probe=0;
@@ -78,8 +78,7 @@ if [ `hostname -s` == "node01" ]; then
       sleep 3
   done
   
-  #docker swarm init --default-addr-pool 10.20.0.0/16 |grep "join --token"
-  
+ 
 else
   echo OK
   #TOKEN=`cat /run/scripts/token.dat |cut -f1 -d";"`
