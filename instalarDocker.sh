c2f6b38ac851b30aff4c6d65ea07cb15cd4d95a7
Waldemar Dibiazi Junior <waldemarjr@gmail.com>
2024-05-11 08:45:14 -0300

    Update instalarDocker.sh

--- instalarDocker.sh
+++ instalarDocker.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-# Instalação de pacote Docker
+#
+# Instalação do Docker
+#
+# Minicurso - Criando um cluster de aplicações utilizando containers Docker. 
+#
+# ETEC de Santa Rosa de Viterbo - 2024
+#
+# por Waldemar Dibiazi Junior
+#
+# Rev. 002
+#
 
 writeLog(){
   echo $1
@@ -14,28 +24,28 @@ check(){
 }
 
 apt update 1> /dev/null 2> /dev/null
-check "update"
+check "Atualizando pacotes do sistema..."
 
 apt install apt-transport-https ca-certificates curl gnupg lsb-release -y > /dev/null 2> /dev/null
-check "install_others"
+check "Instalando pacotes diversos..."
 
 install -m 0755 -d /etc/apt/keyrings 1> /dev/null 2> /dev/null 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg 1> /dev/null 2> /dev/null 
-check "install_gpg_docker_repo"
+check "Instalando chave de acesso a repositorio Docker..."
 
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-check "install_docker_repo"
+check "Instalando as configuracoes do repositorio Docker..."
 
 apt update 1> /dev/null 2> /dev/null
 
 sleep 5
 
 apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin 1> /dev/null 2> /dev/null
-check "install_docker"
+check "Instalando o Docker..."
 
 systemctl enable docker --now
-check "enable_docker"
+check "Ativando o Docker para inicializar durante o boot do sistema..."
